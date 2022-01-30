f59aac7dccdb9d006df34524073005860072c2d8
iapchi <48498436+iapchi@users.noreply.github.com>
2022-01-30 01:53:13 -0500

    ShulkerBoxes protection sign only.

--- Essentials/src/main/java/com/earth2me/essentials/signs/SignProtection.java
+++ Essentials/src/main/java/com/earth2me/essentials/signs/SignProtection.java
@@ -26,7 +26,24 @@ import static com.earth2me.essentials.I18n.tl;
 @Deprecated // This sign will be removed soon
 public class SignProtection extends EssentialsSign {
     private final transient Set<Material> protectedBlocks = EnumUtil.getAllMatching(Material.class,
-        "SHULKER_BOX");
+            "SHULKER_BOX",
+            "WHITE_SHULKER_BOX",
+            "ORANGE_SHULKER_BOX",
+            "MAGENTA_SHULKER_BOX",
+            "LIGHT_BLUE_SHULKER_BOX",
+            "YELLOW_SHULKER_BOX",
+            "LIME_SHULKER_BOX",
+            "PINK_SHULKER_BOX",
+            "GRAY_SHULKER_BOX",
+            "LIGHT_GRAY_SHULKER_BOX",
+            "CYAN_SHULKER_BOX",
+            "PURPLE_SHULKER_BOX",
+            "BLUE_SHULKER_BOX",
+            "BROWN_SHULKER_BOX",
+            "GREEN_SHULKER_BOX",
+            "RED_SHULKER_BOX",
+            "BLACK_SHULKER_BOX"
+    );
 
     public SignProtection() {
         super("Protection");
